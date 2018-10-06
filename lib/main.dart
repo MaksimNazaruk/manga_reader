1cb701a8ac65ff27fb3034fed5b1f28db33a9e87
Maksim Nazaruk <maksim.nazaruk@gmail.com>
2018-10-06 17:17:10 +0300

    tile cell

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,13 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<Widget> _cells = [];
+  List<Manga> _mangaList = [];
 
   @override
   void initState() {
-    _loadList().then((cells) {
+    _loadList().then((mangas) {
       setState(() {
-        _cells = cells;
+        _mangaList = mangas;
       });
     });
     super.initState();
@@ -28,53 +28,87 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Manga Reader',
         theme: ThemeData(
-          brightness: Brightness.dark,
-          primarySwatch: Colors.teal,
-          accentColor: Colors.tealAccent,
-        ),
+            brightness: Brightness.dark,
+            accentColor: Color(0xFFCDDC39),
+            disabledColor: Color(0xFF607D8B),
+            scaffoldBackgroundColor: Color(0xFF455A64)),
         home: Scaffold(
           body: GridView.count(
             crossAxisCount: 2,
-            children: _cells,
+            childAspectRatio: 3.0 / 5.0,
+            children: _cells(),
           ),
         ));
   }
 
-  Future<List<Widget>> _loadList() async {
+  Future<List<Manga>> _loadList() async {
     var requestInfo = RequestInfo.json(
         type: RequestType.get, url: UrlFormatter().listPage(0).toString());
     var response = await BaseService().performRequest(requestInfo);
     List<Manga> mangas = (response["manga"] as List)
         .map((mangaMap) => Manga.fromShortMap(mangaMap))
         .toList();
-    var cells = mangas.map((manga) {
-      return Padding(
-          padding: EdgeInsets.all(4.0),
-          child: Column(
-            children: <Widget>[
-              AspectRatio(
-                  aspectRatio: 5.0 / 3.0,
-                  child: Container(
-                      child: manga.posterUrl != null
-                          ? CachedNetworkImage(
-                              imageUrl: UrlFormatter()
-                                  .image(manga.posterUrl)
-                                  .toString(),
-                              placeholder:
-                                  Center(child: CircularProgressIndicator()),
-                              errorWidget: Center(child: Icon(Icons.error)),
-                            )
-                          : Center(child: Icon(Icons.find_in_page)))),
-              Text(
-                manga.title,
-                maxLines: 2,
-              )
-            ],
-          ));
-    }).toList();
 
-    return cells;
+    return mangas;
+  }
+
+  List<Widget> _cells() {
+    return _mangaList.map((manga) {
+      return TileCell(
+        title: manga.title,
+        posterUrl: manga.posterUrl != null
+            ? UrlFormatter().image(manga.posterUrl).toString()
+            : null,
+        onTap: (() {}),
+      );
+    }).toList();
+  }
+}
+
+class TileCell extends StatelessWidget {
+  final String title;
+  final String posterUrl;
+  final VoidCallback onTap;
+
+  TileCell({this.title, this.posterUrl, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(5.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 3.5 / 5.0,
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(4.0)),
+              child: Container(
+                  color: Theme.of(context).disabledColor,
+                  child: posterUrl != null
+                      ? CachedNetworkImage(
+                          fit: BoxFit.cover,
+                          imageUrl: posterUrl,
+                          placeholder:
+                              Center(child: CircularProgressIndicator()),
+                          errorWidget: Center(child: Icon(Icons.error)),
+                        )
+                      : Center(child: Icon(Icons.find_in_page, size: 64.0,))),
+            ),
+          ),
+          SizedBox(
+            height: 4.0,
+          ),
+          Text(
+            title,
+            maxLines: 2,
+            textAlign: TextAlign.start,
+            overflow: TextOverflow.ellipsis,
+          )
+        ],
+      ),
+    );
   }
 }

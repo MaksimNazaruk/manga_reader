2fd1734ff0b9507d582805ee9562111ef1ddeeed
Maksim Nazaruk <maksim.nazaruk@gmail.com>
2018-10-06 22:11:10 +0300

    Cell design

--- lib/main.dart
+++ lib/main.dart
@@ -32,14 +32,21 @@ class _AppState extends State<App> {
         theme: ThemeData(
             brightness: Brightness.dark,
             accentColor: Color(0xFFCDDC39),
-            disabledColor: Color(0xFF607D8B),
-            scaffoldBackgroundColor: Color(0xFF455A64)),
+            splashColor: Color(0xFFCDDC39),
+            scaffoldBackgroundColor: Color(0xFF455A64),
+            backgroundColor: Color(0xFF607D8B),
+            primaryColor: Color(0xFFA4B02D),
+            disabledColor: Color(0xFFBDBDBD)),
         home: Scaffold(
-          body: GridView.count(
-            crossAxisCount: 2,
-            childAspectRatio: 3.0 / 5.0,
-            children: _cells(),
-          ),
+          body: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
+                childAspectRatio: 3.5 / 5.0,
+                children: _cells(),
+              )),
         ));
   }
 
@@ -76,39 +83,77 @@ class TileCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(5.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 3.5 / 5.0,
-            child: ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(4.0)),
-              child: Container(
-                  color: Theme.of(context).disabledColor,
-                  child: posterUrl != null
-                      ? CachedNetworkImage(
-                          fit: BoxFit.cover,
-                          imageUrl: posterUrl,
-                          placeholder:
-                              Center(child: CircularProgressIndicator()),
-                          errorWidget: Center(child: Icon(Icons.error)),
-                        )
-                      : Center(child: Icon(Icons.find_in_page, size: 64.0,))),
-            ),
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        AspectRatio(
+          aspectRatio: 3.5 / 5.0,
+          child: Container(
+              color: Theme.of(context).disabledColor,
+              child: _posterImage(posterUrl)),
+        ),
+        Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                  height: 50.0,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment(0.0, 1.0),
+                        end: Alignment(0.0, -1.0),
+                        colors: <Color>[
+                          Theme.of(context).primaryColor.withAlpha(245),
+                          Theme.of(context).primaryColor.withAlpha(0)
+                        ],
+                      ),
+                    ),
+                  )),
+              Container(
+                  color: Theme.of(context).primaryColor.withAlpha(245),
+                  child: Padding(
+                      padding: EdgeInsets.all(4.0),
+                      child: Text(
+                        title,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                      ))),
+            ]),
+        Material(
+          color: Colors.transparent,
+          child: InkWell(
+            splashColor: Theme.of(context).splashColor.withAlpha(128),
+            onTap: onTap,
           ),
-          SizedBox(
-            height: 4.0,
-          ),
-          Text(
-            title,
-            maxLines: 2,
-            textAlign: TextAlign.start,
-            overflow: TextOverflow.ellipsis,
-          )
-        ],
-      ),
+        )
+      ],
     );
+  }
+
+  Widget _posterImage(String posterUrl) {
+    return posterUrl != null
+        ? CachedNetworkImage(
+            fit: BoxFit.cover,
+            imageUrl: posterUrl,
+            placeholder: Center(child: CircularProgressIndicator()),
+            errorWidget: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                  Icon(Icons.error, size: 64.0),
+                  SizedBox(height: 4.0),
+                  Text("Couldn't load poster")
+                ])),
+          )
+        : Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Icon(Icons.find_in_page, size: 64.0),
+                SizedBox(height: 4.0),
+                Text("No poster")
+              ]));
   }
 }

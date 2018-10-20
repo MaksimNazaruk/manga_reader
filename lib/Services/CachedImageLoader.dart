aa839b27f19818d40e333a22def3a6fec9245e39
Maksim Nazaruk <maksim.nazaruk@gmail.com>
2018-10-20 23:14:11 +0300

    basic network loader

--- lib/Services/CachedImageLoader.dart
+++ lib/Services/CachedImageLoader.dart
@@ -9,16 +9,34 @@ enum ImageStorage { documents, cache }
 class CachedImageLoader {
   Future<String> _localImageUrl(
       {String shortImageUrl, ImageStorage storage}) async {
-    var cacheDir = await getTemporaryDirectory();
-    return cacheDir.path + shortImageUrl;
+    Directory directory;
+    switch (storage) {
+      case ImageStorage.cache:
+        directory = await getTemporaryDirectory();
+        break;
+      case ImageStorage.documents:
+        directory = await getApplicationDocumentsDirectory();
+        break;
+    }
+    return directory.path + shortImageUrl;
   }
 
-  Future<Uint8List> _loadImage({String imageUrl}) async {
+  Future<Uint8List> _loadLocalImage({String imageUrl}) async {
     var imageData = await File(imageUrl).readAsBytes();
     return Uint8List.fromList(imageData);
   }
 
-  Future<void> _saveImage({String imageUrl, Uint8List imageData}) async {
+  Future<void> _saveLocalImage({String imageUrl, Uint8List imageData}) async {
     await File(imageUrl).writeAsBytes(imageData.toList());
+  }
+
+  Future<Uint8List> _loadNetworkImage({String imageUrl}) async {
+    var request = await HttpClient().getUrl(Uri(path: imageUrl));
+    var response = await request.close();
+    List<int> buffer = [];
+    await for (var data in response) {
+      buffer.addAll(data);
+    }
+    return Uint8List.fromList(buffer);
   }
 }

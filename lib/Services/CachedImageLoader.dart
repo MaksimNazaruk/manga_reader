51553f455f4d0e2dbc900267709bf72643a8c542
Maksim Nazaruk <maksim.nazaruk@gmail.com>
2018-10-22 09:30:39 +0300

    queue loading WiP

--- lib/Services/CachedImageLoader.dart
+++ lib/Services/CachedImageLoader.dart
@@ -6,8 +6,49 @@ import 'package:path_provider/path_provider.dart';
 
 enum ImageStorage { documents, cache }
 
+class _LoadOperation {
+  final Completer<Uint8List> completer;
+  final String url;
+
+  _LoadOperation(this.completer, this.url);
+}
+
 class CachedImageLoader {
-  Future<Uint8List> loadImage({String fullImageUrl}) async {
+  List<_LoadOperation> _queue = [];
+  // _LoadOperation _currentOperation;
+  bool _processingQueue = false;
+
+  Future<Uint8List> loadImage(
+      {String fullImageUrl, bool prioritize = false}) async {
+    _LoadOperation operation =
+        _LoadOperation(Completer<Uint8List>(), fullImageUrl);
+    if (prioritize) {
+      _queue.insert(0, operation);
+    } else {
+      _queue.add(operation);
+    }
+    if (!_processingQueue) {
+      _processingQueue = true;
+      _processQueue();
+    }
+    return operation.completer.future;
+  }
+
+  void _processQueue() async {
+    print("!@! start processing queue");
+    while (_queue.isNotEmpty) {
+      var operation = _queue.first;
+      print("!@! start processing operation ${operation.url}");
+      var imageData = await _loadImage(fullImageUrl: operation.url);
+      print("!@! end processing operation ${operation.url}");
+      operation.completer.complete(imageData);
+      _queue.removeAt(0);
+    }
+    _processingQueue = false;
+    print("!@! end processing queue");
+  }
+
+  Future<Uint8List> _loadImage({String fullImageUrl}) async {
     // print("!@! loading $fullImageUrl");
     var imageName = fullImageUrl.split("/").last;
     var localUrl =
@@ -37,6 +78,7 @@ class CachedImageLoader {
   Future<Uint8List> _loadLocalImage({String imageUrl}) async {
     var file = File(imageUrl);
     var exists = await file.exists();
+    // file.lastModified()
     if (exists) {
       var imageData = await file.readAsBytes();
       return Uint8List.fromList(imageData);
